7120eec58814b57fd3eab0d5c1921ccc42a5f62e
Andrey <Andreysk8er@ukr.net>
2020-12-25 01:27:55 +0300

    moved layout in classes and output to #app

--- src/components/excel/Excel.js
+++ src/components/excel/Excel.js
@@ -1,6 +1,27 @@
+import {$} from "@core/dom";
+
 export class Excel {
     constructor(selector, option) {
         this.$el = document.querySelector(selector);
         this.components = option.components || [];
+    }
+
+    getRoot() {
+        const $root = $.create('div', 'excel');
+
+        this.components.forEach(Component => {
+            const $el = $.create('div', Component.className);
+            const component = new Component($el);
+            $el.html(component.toHTML());
+            console.log($el)
+            $root.append($el);
+
+        });
+        return $root;
+
+    }
+
+    render() {
+        this.$el.append(this.getRoot().$el);
     }
 }
