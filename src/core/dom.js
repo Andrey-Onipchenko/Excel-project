2a6adccb90a13736e1a63e21efe5c97decacadad
Andrey <Andreysk8er@ukr.net>
2021-01-04 00:43:09 +0300

    create universal remove listener

--- src/core/dom.js
+++ src/core/dom.js
@@ -13,14 +13,19 @@ class Dom {
         return this.$el.outerHTML.trim();
     }
 
-    on(eventType, callback) {
-        this.$el.addEventListener(eventType, callback)
-    }
-
     clear() {
         this.html('')
         return this;
     }
+
+    on(eventType, callback) {
+        this.$el.addEventListener(eventType, callback)
+    }
+
+    off(eventType, callback) {
+        this.$el.removeEventListener(eventType, callback)
+    }
+
 
     append(node) {
         if (node instanceof Dom) {
