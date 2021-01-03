9c2a9d0ce86333a8accb85daa40b386c8f89b768
Andrey <Andreysk8er@ukr.net>
2021-01-04 00:21:09 +0300

    create universal listener

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,9 +1,31 @@
+import {capitelize} from "@core/utils";
+
 export class DomListener {
-    constructor($root) {
+    constructor($root, listeners = []) {
         if (!$root) {
             throw  new Error(`no root provided for DomListener`);
         }
         this.$root = $root
+        this.listeners = listeners;
     }
 
+    initDOMListeners() {
+        this.listeners.forEach(listener => {
+            const method = getMethodName(listener);
+            console.log(method)
+            if (!this[method]) {
+                throw  new Error(
+                    `Method ${method} is not implemented in ${this.name} component`
+                )
+            }
+            this.$root.on(listener, this[method].bind(this));
+        })
+    }
+
+    removeDOMListeners() {
+    }
+}
+
+function getMethodName(eventName) {
+    return 'on' + capitelize(eventName)
 }
--- src/core/dom.js
+++ src/core/dom.js
@@ -13,8 +13,8 @@ class Dom {
         return this.$el.outerHTML.trim();
     }
 
-    on() {
-
+    on(eventType, callback) {
+        this.$el.addEventListener(eventType, callback)
     }
 
     clear() {
