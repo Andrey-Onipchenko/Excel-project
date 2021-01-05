bf36c50904181a64cfc027f5828b6a5218c742ac
Andrey <Andreysk8er@ukr.net>
2021-01-05 23:51:56 +0300

    create listener click resize

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -1,26 +1,26 @@
 const CODES = {
-  A: 65,
-  Z: 90
+    A: 65,
+    Z: 90
 }
 
 function toCell() {
-  return `
+    return `
     <div class="cell" contenteditable></div>
   `
 }
 
 function toColumn(col) {
-  return `
+    return `
     <div class="column">
       ${col}
-      <div class="col-resize"></div>
+      <div class="col-resize" data-resize="col"></div>
     </div>
   `
 }
 
 function createRow(index, content) {
-  const resize = index ? '<div class="row-resize"></div>' : ''
-  return `
+    const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
+    return `
     <div class="row">
       <div class="row-info">
         ${index ? index : ''}
@@ -32,29 +32,29 @@ function createRow(index, content) {
 }
 
 function toChar(_, index) {
-  return String.fromCharCode(CODES.A + index)
+    return String.fromCharCode(CODES.A + index)
 }
 
 export function createTable(rowsCount = 15) {
-  const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
-  const rows = []
+    const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
+    const rows = []
 
-  const cols = new Array(colsCount)
-      .fill('')
-      .map(toChar)
-      .map(toColumn)
-      .join('')
-
-  rows.push(createRow(null, cols))
-
-  for (let i = 0; i < rowsCount; i++) {
-    const cells = new Array(colsCount)
+    const cols = new Array(colsCount)
         .fill('')
-        .map(toCell)
+        .map(toChar)
+        .map(toColumn)
         .join('')
 
-    rows.push(createRow(i + 1, cells))
-  }
+    rows.push(createRow(null, cols))
 
-  return rows.join('')
+    for (let i = 0; i < rowsCount; i++) {
+        const cells = new Array(colsCount)
+            .fill('')
+            .map(toCell)
+            .join('')
+
+        rows.push(createRow(i + 1, cells))
+    }
+
+    return rows.join('')
 }
