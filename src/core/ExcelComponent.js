d1176b2410fb2a9647c89c1d316568f26da5f2ae
Andrey <Andreysk8er@ukr.net>
2021-01-09 23:58:55 +0300

    active select

--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -1,21 +1,27 @@
 import {DomListener} from '@core/DomListener'
 
 export class ExcelComponent extends DomListener {
-  constructor($root, options = {}) {
-    super($root, options.listeners)
-    this.name = options.name || ''
-  }
+    constructor($root, options = {}) {
+        super($root, options.listeners)
+        this.name = options.name || '';
 
-  // Возвращает шаблон компонента
-  toHTML() {
-    return ''
-  }
+        this.prepare();
+    }
 
-  init() {
-    this.initDOMListeners()
-  }
+    prepare() {
 
-  destroy() {
-    this.removeDOMListeners()
-  }
+    }
+
+    // Возвращает шаблон компонента
+    toHTML() {
+        return ''
+    }
+
+    init() {
+        this.initDOMListeners()
+    }
+
+    destroy() {
+        this.removeDOMListeners()
+    }
 }
